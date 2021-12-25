df140917b06453c77e2005897e27e7f5d4059d9f
crystalmenthol <wutwut@odds.team>
2021-12-25 15:53:49 +0700

    add axios, fetchData

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { useState, useEffect} from 'react';
 
 function App() {
+
+  const [data, setData] = useState({ Items: [] });
+
+  const fetchData = async () => {
+    const result = await axios(
+      "https://eyvku76ua5.execute-api.ap-southeast-1.amazonaws.com/items"
+    );
+    setData(result.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+        <table>
+          <tbody>
+
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+          {data.Items.map(function (data) {
+            return (
+              <tr key={data.id}>
+                <td>
+                  <p>{data.name}</p>
+                </td>
+                <td>
+                  <p>{data.price}</p>
+                </td>
+              </tr>
+            );
+          })}
+          </tbody>
+        </table>
+      </center>
     </div>
   );
 }
